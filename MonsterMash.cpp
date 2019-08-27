e447455db89fbb66bb01aa8d69e595a509b3e39f
Kevin Le <infernapeman@gmail.com>
2019-08-27 16:21:14 -0600

    Post-repo commit.

--- MonsterMash.cpp
+++ MonsterMash.cpp
@@ -5,7 +5,7 @@
 #include "Monster.h"
 #include "Player.h"
 
-unsigned int GetMonsterCount(const unsigned int maxMonsters)
+void GetMonsterCount(unsigned int *maxMonsters)
 {
 	unsigned int monsterCount = 0;
 	while (true)
@@ -13,16 +13,17 @@ unsigned int GetMonsterCount(const unsigned int maxMonsters)
 		std::cout << "Please enter the initial amount of monsters: ";
 		std::cin >> monsterCount;
 
-		if (std::cin.fail() || monsterCount < 0 || monsterCount > maxMonsters)
+		if (std::cin.fail() || monsterCount < 0 || monsterCount > UINT_MAX - 10)
 		{
 			std::cin.clear();
 			std::cin.ignore(INT_MAX, '\n');
-			std::cout << "Invalid input. Please try entering an positive integer, or enter in a number less than " << maxMonsters << ".\n";
+			std::cout << "Invalid input. Please try entering an positive integer.\n";
 		}
 		else
 		{
 			std::cin.ignore(INT_MAX, '\n');
-			return monsterCount;
+			*maxMonsters = monsterCount;
+			break;
 		}
 	}
 }
@@ -45,9 +46,9 @@ void GetName(char *name)
 
 }
 
-void MonsterPrint(Monster* monsters, const unsigned int maxMonsters)
+void MonsterPrint(Monster* monsters, unsigned int* maxMonsters)
 {
-	for (unsigned int i = 0; i < maxMonsters; i++)
+	for (unsigned int i = 0; i < *maxMonsters + 10; i++)
 	{
 		if (monsters[i].isInit())
 		{
@@ -60,9 +61,9 @@ void MonsterPrint(Monster* monsters, const unsigned int maxMonsters)
 
 }
 
-void MoveMonsters(const int maxMonsters, Monster* monsters, Player* player)
+void MoveMonsters(unsigned int* maxMonsters, Monster* monsters, Player* player)
 {
-	for (int i = 0; i < maxMonsters; i++)
+	for (unsigned int i = 0; i < *maxMonsters; i++)
 	{
 		if (monsters[i].isInit())
 		{
@@ -96,9 +97,9 @@ void MovePlayerMain(char movement, Player* player, const int playX, const int pl
 	}
 }
 
-void SpawnMonsters(Monster* monsters, const int maxMonsters, const int playX, const int playY)
+void SpawnMonsters(Monster* monsters, unsigned int* maxMonsters, const int playX, const int playY)
 {
-	for (int i = 0; i < maxMonsters; i++)
+	for (unsigned int i = 0; i < *maxMonsters + 10; i++)
 	{
 		if (monsters[i].DecayTime() == 0)
 		{
@@ -116,6 +117,7 @@ void SpawnMonsters(Monster* monsters, const int maxMonsters, const int playX, co
 				strcat_s(beginning, buffer);
 				monsters[i].SetName(beginning);
 				monsters[i].SetTime();
+				std::cout << monsters[i].GetName() << " has spawned!\n";
 			}
 			if (monsters[i].isInit() == false)
 			{
@@ -126,12 +128,12 @@ void SpawnMonsters(Monster* monsters, const int maxMonsters, const int playX, co
 	}
 }
 
-bool CheckPlayer(Player* player, Monster* monsters, const int maxMonsters)
+bool CheckPlayer(Player* player, Monster* monsters, unsigned int* maxMonsters)
 {
 	int playerX = player->GetX();
 	int playerY = player->GetY();
 
-	for (int i = 0; i < maxMonsters; i++)
+	for (unsigned int i = 0; i < *maxMonsters; i++)
 	{
 		if (monsters[i].isInit() && monsters[i].GetX() == playerX && monsters[i].GetY() == playerY)
 		{
@@ -141,10 +143,8 @@ bool CheckPlayer(Player* player, Monster* monsters, const int maxMonsters)
 	return true;
 }
 
-void GameLoop(Monster* monsters, Player* player, const int maxMonsters, const int playX, const int playY)
+void GameLoop(Monster* monsters, Player* player, unsigned int* maxMonsters, const int playX, const int playY)
 {
-	//Initial startup coverage.
-
 	while (true)
 	{
 		MonsterPrint(monsters, maxMonsters);
@@ -166,14 +166,13 @@ void GameLoop(Monster* monsters, Player* player, const int maxMonsters, const in
 	}
 }
 
-Monster* MonsterCreateLoop(int playX, int playY, int* const count, const unsigned int maxMonsters)
+Monster* MonsterCreateLoop(int playX, int playY, unsigned int* maxMonsters)
 {
 	unsigned int i = 0;
-	unsigned int counts = GetMonsterCount(maxMonsters);
-	*count = counts;
-	std::cout << counts << " is the initial amount of monsters.\n";
-	Monster* monsters = new Monster[maxMonsters];
-	for (; i < counts; i++)
+	GetMonsterCount(maxMonsters);
+	std::cout << *maxMonsters << " is the initial amount of monsters.\n";
+	Monster* monsters = new Monster[*maxMonsters + 10];
+	for (; i < *maxMonsters; i++)
 	{
 		int monX = rand() / (RAND_MAX / playX * 2) - playX;
 		int monY = rand() / (RAND_MAX / playY * 2) - playY;
@@ -182,7 +181,7 @@ Monster* MonsterCreateLoop(int playX, int playY, int* const count, const unsigne
 		GetName(name);
 		monsters[i] = Monster(monX, monY, name);
 	}
-	for (; i < maxMonsters; i++)
+	for (; i < *maxMonsters + 10; i++)
 	{
 		monsters[i] = Monster();
 	}
@@ -205,27 +204,14 @@ int main()
 {
 	static const int playX = 10;
 	static const int playY = 10;
-	static const unsigned int maxMonsters = 16;
-	int* const count = new int;
+	unsigned int monsterCount = 0;
+	unsigned int *maxMonsters = &monsterCount;
 	std::cout << "Monster Mash by Kevin Le (u0916211)\n";
-	Monster* monsters = MonsterCreateLoop(playX, playY, count, maxMonsters);
+	Monster* monsters = MonsterCreateLoop(playX, playY, maxMonsters);
 	Player* player = CreatePlayer(playX, playY);
 
 	GameLoop(monsters, player, maxMonsters, playX, playY);
 
-	delete count;
-
 	delete[] monsters;
 	delete player;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
