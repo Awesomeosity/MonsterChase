fa2e1f79b1aa291c6c1acdea3ff74138afac2fbb
Awesomeosity <lekev001@gmail.com>
2019-09-11 15:32:26 -0600

    Used initialization list instead of initializing in ctor.

--- Engine/GameObject.cpp
+++ Engine/GameObject.cpp
@@ -2,24 +2,20 @@
 #include "GameObject.h"
 #include <iostream>
 
-GameObject::GameObject()
+GameObject::GameObject() : _point()
 {
-	_point = Point2D();
 	_name = new char[1];
 	_name[0] = '\n';
 }
 
-GameObject::GameObject(Point2D point, char* name)
+GameObject::GameObject(Point2D point, char* name) : _point(point)
 {
-	_point = point;
 	_name = new char[strlen(name) + 1];
 	strcpy_s(_name, sizeof(char) * (strlen(name) + 1), name);
 }
 
-GameObject::GameObject(GameObject& gameObject)
+GameObject::GameObject(GameObject& gameObject) : _point(*(gameObject.GetPoint()))
 {
-	_point = *(gameObject.GetPoint());
-
 	char* name = gameObject.GetName();
 	_name = new char[strlen(name) + 1];
 	strcpy_s(_name, sizeof(char) * (strlen(name) + 1), name);
