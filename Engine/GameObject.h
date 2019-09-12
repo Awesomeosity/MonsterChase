6dfe6121b1b9a0b2e44bd6846b09482c959e0871
Awesomeosity <lekev001@gmail.com>
2019-09-11 21:29:36 -0600

    Moved public functions to top of file.

--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -2,18 +2,18 @@
 #include "Point2D.h"
 class GameObject
 {
-private:
-	Point2D _point;
-	char* _name;
 public:
 	GameObject();
-	GameObject(Point2D point, char *name);
+	GameObject(Point2D point, char* name);
 	GameObject(GameObject& gameObject);
 	GameObject& operator=(GameObject& gameObject);
 	~GameObject();
 	char* GetName();
-	void SetName(char *name);
+	void SetName(char* name);
 	Point2D* GetPoint();
 	void SetPoint(Point2D point);
+private:
+	Point2D _point;
+	char* _name;
 };
 
