6bb6ddb6106bb8fdce55a428f4c83897320806f7
Awesomeosity <lekev001@gmail.com>
2019-11-03 12:53:32 -0700

    Removed name from GameObject.

--- Engine/GameObject.cpp
+++ Engine/GameObject.cpp
@@ -4,21 +4,14 @@
 
 GameObject::GameObject() : _point()
 {
-	_name = new char[1];
-	_name[0] = '\n';
 }
 
-GameObject::GameObject(Point2D point, char* name) : _point(point)
+GameObject::GameObject(Point2D point) : _point(point)
 {
-	_name = new char[strlen(name) + 1];
-	strcpy_s(_name, sizeof(char) * (strlen(name) + 1), name);
 }
 
 GameObject::GameObject(GameObject& gameObject) : _point(*(gameObject.GetPoint()))
 {
-	char* name = gameObject.GetName();
-	_name = new char[strlen(name) + 1];
-	strcpy_s(_name, sizeof(char) * (strlen(name) + 1), name);
 }
 
 GameObject& GameObject::operator=(GameObject& gameObject)
@@ -30,27 +23,11 @@ GameObject& GameObject::operator=(GameObject& gameObject)
 
 	_point = *(gameObject.GetPoint());
 
-	char* name = gameObject.GetName();
-	_name = new char[strlen(name) + 1];
-	strcpy_s(_name, sizeof(char) * (strlen(name) + 1), name);
 	return *this;
 }
 
 GameObject::~GameObject()
 {
-	delete[] _name;
-}
-
-char* GameObject::GetName()
-{
-	return _name;
-}
-
-void GameObject::SetName(char* name)
-{
-	delete[] _name;
-	_name = new char[strlen(name) + 1];
-	strcpy_s(_name, sizeof(char) * (strlen(name) + 1), name);
 }
 
 Point2D* GameObject::GetPoint()
--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -4,16 +4,13 @@ class GameObject
 {
 public:
 	GameObject();
-	GameObject(Point2D point, char* name);
+	GameObject(Point2D point);
 	GameObject(GameObject& gameObject);
 	GameObject& operator=(GameObject& gameObject);
 	~GameObject();
-	char* GetName();
-	void SetName(char* name);
 	Point2D* GetPoint();
 	void SetPoint(Point2D point);
 private:
 	Point2D _point;
-	char* _name;
 };
 
