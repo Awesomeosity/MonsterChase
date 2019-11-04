9acff08054891a736968d6b81a8ab76372aa991b
Awesomeosity <lekev001@gmail.com>
2019-11-03 18:20:09 -0700

    Changing Player to fit GameObject's lack of name.

--- MonsterChase/Player.cpp
+++ MonsterChase/Player.cpp
@@ -2,7 +2,7 @@
 #include "../Engine/Point2D.h"
 #include "../Engine/GameObject.h"
 
-Player::Player(Point2D point, char name[]) : GameObject(point, name)
+Player::Player(Point2D point) : GameObject(point)
 {
 }
 
