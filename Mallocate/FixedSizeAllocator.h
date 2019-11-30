02555f8b9cd41ca68d40bc4e93c99f373a34bbee
Awesomeosity <lekev001@gmail.com>
2019-11-30 15:26:59 -0700

    Initializing FixedSizeAllocator class

---
+++ Mallocate/FixedSizeAllocator.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "BitArray.h"
+#include "HeapManagerProxy.h"
+class FixedSizeAllocator
+{
+public:
+	static void Create(size_t i_blockSize, size_t i_blockCount, HeapManager* i_pHeap);
+	~FixedSizeAllocator();
+	void* alloc(size_t i_size);
+	bool free(void* const i_ptr);
+	size_t getBlockSize();
+private:
+	size_t blockSize;
+	BitArray* bitArray;
+};
+
