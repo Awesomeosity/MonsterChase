db9545355eb7f991ce1aeecd598cd9a33e54a3cd
Awesomeosity <lekev001@gmail.com>
2019-12-02 13:00:31 -0700

    Starting to implement FixedSizeAllocator

--- Mallocate/FixedSizeAllocator.cpp
+++ Mallocate/FixedSizeAllocator.cpp
@@ -1,1 +1,12 @@
 #include "FixedSizeAllocator.h"
+#include "HeapManagerProxy.h"
+#include "BitArray.h"
+FixedSizeAllocator* FixedSizeAllocator::Create(size_t i_blockSize, size_t i_blockCount, HeapManager* i_pHeap)
+{
+	//Request allocation of size equal to that of each (block size + guard band size) * block count
+	size_t totalAllocSize = i_blockSize + sizeof(int) * i_blockCount;
+	FixedSizeAllocator* newAlloc = reinterpret_cast<FixedSizeAllocator*>(HeapManagerProxy::alloc(i_pHeap, totalAllocSize));
+	
+	//Must create BitArray outside of this function
+	return newAlloc;
+}
--- Mallocate/FixedSizeAllocator.h
+++ Mallocate/FixedSizeAllocator.h
@@ -4,10 +4,10 @@
 class FixedSizeAllocator
 {
 public:
-	static void Create(size_t i_blockSize, size_t i_blockCount, HeapManager* i_pHeap);
+	static FixedSizeAllocator* Create(size_t i_blockSize, size_t i_blockCount, HeapManager* i_pHeap);
 	~FixedSizeAllocator();
 	void* alloc(size_t i_size);
-	bool free(void* const i_ptr);
+	void free(void* const i_ptr);
 	size_t getBlockSize();
 private:
 	size_t blockSize;
