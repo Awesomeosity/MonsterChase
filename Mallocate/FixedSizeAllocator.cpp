3dc2602309edf604292e30284e5fd353d291c27a
Awesomeosity <lekev001@gmail.com>
2019-12-07 17:13:16 -0700

    Adding small change to set up FSA's BitArray

--- Mallocate/FixedSizeAllocator.cpp
+++ Mallocate/FixedSizeAllocator.cpp
@@ -6,7 +6,8 @@ FixedSizeAllocator* FixedSizeAllocator::Create(size_t i_blockSize, size_t i_bloc
 	//Request allocation of size equal to that of each (block size + guard band size) * block count
 	size_t totalAllocSize = sizeof(FixedSizeAllocator) + (i_blockSize + sizeof(int)) * i_blockCount;
 	FixedSizeAllocator* newAlloc = reinterpret_cast<FixedSizeAllocator*>(HeapManagerProxy::alloc(i_pHeap, totalAllocSize));
-	
+	newAlloc->blockSize = i_blockSize;
+	newAlloc->bitArray = BitArray::Create()
 	//Must create BitArray outside of this function
 	return newAlloc;
 }
