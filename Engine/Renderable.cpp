49d44c9aac851ec49d08d442c620563bbaf30a08
Awesomeosity <lekev001@gmail.com>
2020-02-16 20:06:49 -0700

    Renderable class added

---
+++ Engine/Renderable.cpp
@@ -0,0 +1,21 @@
+#include "Renderable.h"
+
+void Renderable::AddRenderable(SmartPointer<GameObject> gameObj, GLib::Sprites::Sprite* sprPtr)
+{
+	renderables.push_back(renders(WeakPointer<GameObject>(gameObj), sprPtr));
+}
+
+void Renderable::RenderAll()
+{
+	for (int i = 0; i < renderables.size(); i++)
+	{
+		float p_x = renderables[i].gameObj->GetPoint()->GetX();
+		float p_y = renderables[i].gameObj->GetPoint()->GetY();
+		float spritePos_X = p_x * 50;
+		float spritePos_Y = p_y * 50;
+
+		GLib::Point2D	Offset = { spritePos_X, spritePos_Y };
+
+		GLib::Sprites::RenderSprite(*(renderables[i].sprPtr), Offset, 0);
+	}
+}
---
+++ Engine/Renderable.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "GLib/BasicTypes.h"
+#include "GLib/GLib.h"
+#include "Objects/WeakPointer.h"
+#include "Objects/GameObject.h"
+#include <vector>
+struct renders
+{
+	WeakPointer<GameObject> gameObj;
+	GLib::Sprites::Sprite* sprPtr;
+	renders(WeakPointer<GameObject> _obj, GLib::Sprites::Sprite* _ptr)
+		: gameObj(_obj), sprPtr(_ptr)
+	{
+
+	}
+};
+class Renderable
+{
+public:
+	void AddRenderable(SmartPointer<GameObject> gameObj, GLib::Sprites::Sprite* sprPtr);
+	void RenderAll();
+private:
+	std::vector<renders> renderables;
+};
+
