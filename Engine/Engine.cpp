a0ccd67d847a45b3a95ba59f9f28670193a8ec58
Awesomeosity <lekev001@gmail.com>
2020-02-16 20:06:05 -0700

    Engine overhaul.

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -15,10 +15,14 @@
 
 #include "External/nlohmann/json.hpp"
 #include "Objects/GameObject.h"
-#include "../MonsterChase/Controllers/PlayerController.h"
-
 
 unsigned int currKey = 0;
+Physics physSystem;
+
+void initEngine()
+{
+	physSystem = Physics();
+}
 
 void TestKeyCallback(unsigned int i_VKeyID, bool bWentDown)
 {
@@ -181,120 +185,149 @@ void GetName(char* name)
 	}
 }
 
-SmartPointer<GameObject> CreateActor(const char* i_pScriptFilename)
+SmartPointer<GameObject> CreateActor(const char* i_pScriptFilename, int& o_controllerType)
 {
 	using json = nlohmann::json;
 	std::vector<uint8_t> data = LoadFileToBuffer(i_pScriptFilename);
 
 	if (!data.empty())
 	{
-		json createdJSON = json::parse(data);
-		//TODO create structure for json decision tree
-		//TODO verification
-		//TODO return SmartPointer
+		json obJSON = json::parse(data);
+
+		assert(obJSON.contains("position"));
+		assert(obJSON["position"].is_array());
+		assert(obJSON["position"].size() == 2);
+		Point2D* actorPos = new Point2D(obJSON["position"][0], obJSON["position"][1]);
+
+		GameObject* actorObj = new GameObject(*actorPos);
+		SmartPointer<GameObject> actorSmPtr = SmartPointer<GameObject>(actorObj);
+
+		if (obJSON.contains("collision_data"))
+		{
+			assert(obJSON["collision_data"]["mass"].is_number_float());
+			assert(obJSON["collision_data"]["kd"].is_number_float());
+			float actorMass = obJSON["collision_data"]["mass"];
+			float actorKD = obJSON["collision_data"]["kd"];
+
+			physSystem.AddCollidableObject(actorSmPtr, actorMass, actorKD);
+		}
+
+		if (obJSON.contains("render_data"))
+		{
+			assert(obJSON["render_data"]["sprite"].is_string());
+
+			std::string Sprite = obJSON["render_data"]["sprite"];
+
+			//TODO: Add renderable classes
+		}
+
+		assert(obJSON.contains("name"));
+		assert(obJSON["name"].is_string());
+		std::string name = obJSON["name"];
+		char* name_c = const_cast<char*>(name.c_str());
+
+
+		if (obJSON.contains("controller"))
+		{
+			assert(obJSON["controller"].is_string());
+			std::string conType = obJSON["controller"];
+		
+			if (conType == "player")
+			{
+				o_controllerType = 0;
+			}
+			else if (conType == "normal")
+			{
+				o_controllerType = 1;
+			}
+			else if (conType == "random")
+			{
+				o_controllerType = 2;
+			}
+			else
+			{
+				o_controllerType = -1;
+			}
+		}
+
+		return actorSmPtr;
 	}
 }
 
 void Run(HINSTANCE i_hInstance, int i_nCmdShow)
 {
-	float playX = 10.0f;
-	float playY = 10.0f;
 
-	unsigned short ID = 65535;
+	//GLib::Sprites::Sprite* pGoodGuy = CreateSprite("data\\GoodGuy.dds");
+	GLib::SetKeyStateChangeCallback(TestKeyCallback);
 
-	Point2D* zero = new Point2D(0, 0);
-	GameObject* playerObj = new GameObject(*zero);
+	Timing::startTime();
 
-	PlayerController* player = new PlayerController();
+	bool bQuit = false;
 
-	PhysicsData* playerPhysics = new PhysicsData(playerObj, 1, 0);
-
-	player->SetGameObject(playerObj);
-	player->Setup(const_cast<char*>("lmao"), playX, playY);
-
-
-	// IMPORTANT: first we need to initialize GLib
-	bool bSuccess = GLib::Initialize(i_hInstance, i_nCmdShow, "Monster Mash", ID, static_cast<unsigned int>(playX) * 50 * 2, static_cast<unsigned int>(playY) * 50 * 2);
-
-	while (1)
+	do
 	{
-		if (bSuccess)
+		//Timing
+		long dt = Timing::deltaTime();
+		float dt_ms = (float)dt / 1000.0f;
+
+		GLib::Service(bQuit);
+
+		if (!bQuit)
 		{
-			GLib::SetKeyStateChangeCallback(TestKeyCallback);
-
-			GLib::Sprites::Sprite* pGoodGuy = CreateSprite("data\\GoodGuy.dds");
-
-			Timing::startTime();
-
-			bool bQuit = false;
-
-			do
+			Point2D force;
+			switch (currKey)
 			{
-				//Timing
-				long dt = Timing::deltaTime();
-				float dt_ms = (float)dt / 1000.0f;
+				//W
+			case 87:
+				force = Point2D(0, 1);
+				break;
+				//S
+			case 83:
+				force = Point2D(0, -1);
+				break;
+				//A
+			case 65:
+				force = Point2D(-1, 0);
+				break;
+				//D
+			case 68:
+				force = Point2D(1, 0);
+				break;
+			case 81:
+				break;
+				//No key being held down.
+			case 0:
+			default:
+				force = Point2D(0, 0);
+				break;
+			}
+			//Reset key press each frame;
+			currKey = 0;
 
-				GLib::Service(bQuit);
+			//Physics
+			Physics::calcNewPos(dt_ms, playerPhysics, force);
 
-				if (!bQuit)
-				{
-					Point2D force;
-					switch (currKey)
-					{
-						//W
-					case 87:
-						force = Point2D(0, 1);
-						break;
-						//S
-					case 83:
-						force = Point2D(0, -1);
-						break;
-						//A
-					case 65:
-						force = Point2D(-1, 0);
-						break;
-						//D
-					case 68:
-						force = Point2D(1, 0);
-						break;
-					case 81:
-						break;
-						//No key being held down.
-					case 0:
-					default:
-						force = Point2D(0, 0);
-						break;
-					}
-					//Reset key press each frame;
-					currKey = 0;
+			//Render
+			GLib::BeginRendering();
+			GLib::Sprites::BeginRendering();
+			if (pGoodGuy)
+			{
+				float p_x = player->getPlayerPosition()->GetX();
+				float p_y = player->getPlayerPosition()->GetY();
+				float playerSpritePos_X = p_x * 50;
+				float playerSpritePos_Y = p_y * 50;
 
-					//Physics
-					Physics::calcNewPos(dt_ms, playerPhysics, force);
+				GLib::Point2D	Offset = { playerSpritePos_X, playerSpritePos_Y };
 
-					//Render
-					GLib::BeginRendering();
-					GLib::Sprites::BeginRendering();
-					if (pGoodGuy)
-					{
-						float p_x = player->getPlayerPosition()->GetX();
-						float p_y = player->getPlayerPosition()->GetY();
-						float playerSpritePos_X = p_x * 50;
-						float playerSpritePos_Y = p_y * 50;
+				GLib::Sprites::RenderSprite(*pGoodGuy, Offset, 0);
+			}
 
-						GLib::Point2D	Offset = { playerSpritePos_X, playerSpritePos_Y };
-
-						GLib::Sprites::RenderSprite(*pGoodGuy, Offset, 0);
-					}
-
-					GLib::Sprites::EndRendering();
-					GLib::EndRendering();
-				}
-
-			} while (bQuit == false);
-
-			// IMPORTANT:  Tell GLib to shutdown, releasing resources.
-			GLib::Shutdown();
+			GLib::Sprites::EndRendering();
+			GLib::EndRendering();
 		}
 
-	}
+	} while (bQuit == false);
+
+	// IMPORTANT:  Tell GLib to shutdown, releasing resources.
+	GLib::Shutdown();
 }
