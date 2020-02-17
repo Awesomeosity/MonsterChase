fdf4d7304da6eabe80090be67bdf276c10019211
Awesomeosity <lekev001@gmail.com>
2020-02-16 20:07:23 -0700

    Physics Data modification.

--- Engine/Physics/PhysicsData.cpp
+++ Engine/Physics/PhysicsData.cpp
@@ -1,29 +1,18 @@
 #include "PhysicsData.h"
 #include "../Objects/GameObject.h"
 PhysicsData::PhysicsData() :
-	object(nullptr), mass(0), drag(0), prevPos()
+	mass(0), drag(0), prevPos()
 {
 }
 
-PhysicsData::PhysicsData(GameObject* _object, float _mass, float _drag) :
-	object(_object), mass(_mass), drag(_drag), prevPos()
+PhysicsData::PhysicsData(float _mass, float _drag) :
+	mass(_mass), drag(_drag), prevPos()
 {
 }
 
 Point2D PhysicsData::GetPrevPos()
 {
 	return prevPos;
-}
-//Updates the previous position of this object
-void PhysicsData::UpdatePrevPos(Point2D point)
-{
-	prevPos = *((*object).GetPoint());
-	(*object).SetPoint(point);
-}
-
-Point2D PhysicsData::GetCurrentPos()
-{
-	return *(*object).GetPoint();
 }
 
 float PhysicsData::GetMass()
--- Engine/Physics/PhysicsData.h
+++ Engine/Physics/PhysicsData.h
@@ -5,15 +5,12 @@ class PhysicsData
 {
 public:
 	PhysicsData();
-	PhysicsData(GameObject* _object, float _mass, float _drag);
+	PhysicsData(float _mass, float _drag);
 
 	Point2D GetPrevPos();
-	void UpdatePrevPos(Point2D point);
-	Point2D GetCurrentPos();
 	float GetMass();
 	float GetDrag();
 private:
-	GameObject* object;
 	float mass;
 	float drag;
 	Point2D prevPos;
