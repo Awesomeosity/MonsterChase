454678b8cba5701930021fb24fbe2844c12930b5
Awesomeosity <lekev001@gmail.com>
2020-04-25 18:36:24 -0600

    fixes to loadfile

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -96,94 +96,6 @@ namespace Engine
 		(*forcePtr).SetY(tempForces.GetY());
 	}
 
-	void* LoadFile(const char* i_pFilename, size_t& o_sizeFile)
-	{
-		assert(i_pFilename != NULL);
-
-		FILE* pFile = NULL;
-
-		errno_t fopenError = fopen_s(&pFile, i_pFilename, "rb");
-		if (fopenError != 0)
-			return NULL;
-
-		assert(pFile != NULL);
-
-		int FileIOError = fseek(pFile, 0, SEEK_END);
-		assert(FileIOError == 0);
-
-		long FileSize = ftell(pFile);
-		assert(FileSize >= 0);
-
-		FileIOError = fseek(pFile, 0, SEEK_SET);
-		assert(FileIOError == 0);
-
-		uint8_t* pBuffer = new uint8_t[FileSize];
-		assert(pBuffer);
-
-#if _DEBUG
-		size_t FileRead = fread(pBuffer, 1, FileSize, pFile);
-		assert(FileRead == (size_t)FileSize);
-#endif
-		fclose(pFile);
-
-		o_sizeFile = FileSize;
-
-		return pBuffer;
-	}
-
-
-	GLib::Sprite* CreateSprite(const char* i_pFilename)
-	{
-		assert(i_pFilename);
-
-		size_t sizeTextureFile = 0;
-
-		// Load the source file (texture data)
-		void* pTextureFile = LoadFile(i_pFilename, sizeTextureFile);
-
-		// Ask GLib to create a texture out of the data (assuming it was loaded successfully)
-		GLib::Texture* pTexture = pTextureFile ? GLib::CreateTexture(pTextureFile, sizeTextureFile) : nullptr;
-
-		// exit if something didn't work
-		// probably need some debug logging in here!!!!
-		if (pTextureFile)
-			delete[] pTextureFile;
-
-		if (pTexture == nullptr)
-			return NULL;
-
-		unsigned int width = 0;
-		unsigned int height = 0;
-		unsigned int depth = 0;
-
-#if _DEBUG
-		// Get the dimensions of the texture. We'll use this to determine how big it is on screen
-		bool result = GLib::GetDimensions(*pTexture, width, height, depth);
-		assert(result == true);
-		assert((width > 0) && (height > 0));
-#else
-		GLib::GetDimensions(*pTexture, width, height, depth);
-#endif
-
-		// Define the sprite edges
-		GLib::SpriteEdges	Edges = { -float(width / 2.0f), float(height), float(width / 2.0f), 0.0f };
-		GLib::SpriteUVs	UVs = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } };
-		GLib::RGBA							Color = { 255, 255, 255, 255 };
-
-		// Create the sprite
-		GLib::Sprite* pSprite = GLib::CreateSprite(Edges, 0.1f, Color, UVs);
-		if (pSprite == nullptr)
-		{
-			GLib::Release(pTexture);
-			return nullptr;
-		}
-
-		// Bind the texture to sprite
-		GLib::SetTexture(*pSprite, pTexture);
-
-		return pSprite;
-	}
-
 	std::vector<uint8_t> LoadFileToBuffer(std::string i_pFilename)
 	{
 		//assert(i_pFilename != nullptr);
@@ -218,6 +130,54 @@ namespace Engine
 		}
 
 		return Buffer;
+	}
+
+
+	GLib::Sprite* CreateSprite(std::string i_pFilename)
+	{
+
+		size_t sizeTextureFile = 0;
+
+		// Load the source file (texture data)
+		std::vector<uint8_t> pTextureFile = LoadFileToBuffer(i_pFilename.c_str());
+
+		// Ask GLib to create a texture out of the data (assuming it was loaded successfully)
+		//GLib::Texture* pTexture = pTextureFile ? GLib::CreateTexture(pTextureFile, sizeTextureFile) : nullptr;
+		GLib::Texture* pTexture = GLib::CreateTexture(&pTextureFile[0], pTextureFile.size());
+
+		if (pTexture == nullptr)
+		{
+			OutputDebugStringA("Texture loading failed.\n");
+			return NULL;
+		}
+		else
+		{
+			unsigned int width = 0;
+			unsigned int height = 0;
+			unsigned int depth = 0;
+
+			bool result = GLib::GetDimensions(*pTexture, width, height, depth);
+			assert(result == true);
+			assert((width > 0 && height > 0));
+
+			// Define the sprite edges
+			GLib::SpriteEdges	Edges = { -float(width / 2.0f), float(height), float(width / 2.0f), 0.0f };
+			GLib::SpriteUVs	UVs = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } };
+			GLib::RGBA							Color = { 255, 255, 255, 255 };
+
+			// Create the sprite
+			GLib::Sprite* pSprite = GLib::CreateSprite(Edges, 0.1f, Color, UVs);
+			if (pSprite == nullptr)
+			{
+				GLib::Release(pTexture);
+				return nullptr;
+			}
+
+			// Bind the texture to sprite
+			GLib::SetTexture(*pSprite, pTexture);
+
+			return pSprite;
+		}
 	}
 
 	void GetName(char* name)
@@ -278,7 +238,7 @@ namespace Engine
 
 			std::string sprite = obJSON["render_data"]["sprite"];
 
-			GLib::Sprite* pSprite = CreateSprite(sprite.c_str());
+			GLib::Sprite* pSprite = CreateSprite(sprite);
 			renderSystem->AddRenderable(actorPtr, pSprite);
 		}
 
